60da37c6c6f0f47ab88df8fc8164fbe4fcde5ac2
James McDowell <jamesnmcdowell@gmail.com>
2018-10-06 21:03:46 -0400

    finish quick homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Header from './Header';
 import Shell from './Shell';
+import llama from './llama1.jpg';
+import llama2 from './llama2.jpg'
 
 let Homepage = () =>
     <Shell>
         <div className="homepage-grid"> 
             <div className="hero">
                 <h1> Need More Llama ... No Problama </h1>
-                <h1> A Whole lotta Llama ... Without the Drama </h1>
             </div>
-            <div className="hgrid-1">
-                <img src="http://via.placeholder.com/400x400"/>
+            <div className="hgrid-2">
+                <img src={llama} />
             </div>   
+            <div className="hgrid-1">
+                <img src={llama2}/>
+            </div>   
+            <div className="hero">
+                <h1> Explore Endless Llama possibilities</h1>
+            </div>
         </div>
     </Shell>
 export default Homepage;
